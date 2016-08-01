58ac4fb84421d3660f2a14b9e1ac3f791c9fff6a
Luis Rascao <luis.rascao@gmail.com>
2016-08-01 11:16:56 +0100

    Provide gen_server automatic state record migration on upgrade/downgrade
    This is achieved by injecting code into the code_change
    gen_server method, this code will convert between the
    different record versions.

--- src/rebar3_appup_plugin.erl
+++ src/rebar3_appup_plugin.erl
@@ -7,4 +7,5 @@ init(State0) ->
     {ok, State1} = rebar3_appup_generate:init(State0),
     {ok, State2} = rebar3_appup_compile:init(State1),
     {ok, State3} = rebar3_appup_clean:init(State2),
-    {ok, State3}.
+    {ok, State4} = rebar3_appup_tar:init(State3),
+    {ok, State4}.
